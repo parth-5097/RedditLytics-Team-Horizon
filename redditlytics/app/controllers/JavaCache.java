bcbb7864a73305f328a3259b33e170ee4a0e3170
BORAD <jigarborad314@gmail.com>
2021-11-13 22:53:43 -0500

    1 file changed

--- redditlytics/app/controllers/JavaCache.java
+++ redditlytics/app/controllers/JavaCache.java
@@ -9,9 +9,16 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.Optional;
 
-
+/**
+ * <p>The class is defined to maintain a cache during a session.</p>
+ * <p>There are three methods are implemented in class get,set and remove.</p>
+ * <p>AsyncCacheApi interface is implemented in class to achieve caching in web application.</p>
+ *
+ * @author Group development
+ */
 public abstract class JavaCache implements AsyncCacheApi {
     private HashMap<String, Object> cache = new HashMap();
+
 
     @Override
     public <T> CompletionStage<Optional<T>> get(String key) {
